f2837965041be7eaba7b91143fa2675b3ef9afbb
drewdroid86 <jdrew2517@gmail.com>
2026-04-21 23:29:52 -0400

    Initialize project and add Android build workflow

---
+++ android/app/src/main/java/com/engineering/toolbox/MainActivity.java
@@ -0,0 +1,5 @@
+package com.engineering.toolbox;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.engineering.toolbox',
+  appName: 'Engineering Toolbox',
+  webDir: 'dist'
+};
+
+export default config;
